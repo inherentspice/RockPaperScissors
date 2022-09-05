c9c436b2f3f58b174eef6a52822810119179bbc6
Stephen Smith <stephenlehanesmith@gmail.com>
2022-09-05 02:05:53 -0700

    moved logic to update paragraph to seperate function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -45,23 +45,23 @@ function game(playerSelection) {
   return [playRound(playerSelection, computerSelection), computerSelection];
 }
 
-// function updateResults() {
+function updateResults(res, text) {
+  let resultsDiv = document.getElementsByClassName('results')[0];
+  let para = document.createElement('p');
+  let node = document.createTextNode(
+    `You chose: ${text}. The computer chose: ${res[1]}. ${res[0]}`
+    );
+  para.appendChild(node);
+  resultsDiv.appendChild(para);
+}
 
-// }
 function startGame() {
   const buttons = document.querySelectorAll('button');
 
   buttons.forEach((btn) =>
     btn.addEventListener('click', () => {
       res = game(btn.innerText);
-
-      let resultsDiv = document.getElementsByClassName('results')[0];
-      let para = document.createElement('p');
-      let node = document.createTextNode(
-        `You chose: ${btn.innerText}. The computer chose: ${res[1]}. ${res[0]}`
-        );
-      para.appendChild(node);
-      resultsDiv.appendChild(para);
+      updateResults(res, btn.innerText)
     }
     )
   );
